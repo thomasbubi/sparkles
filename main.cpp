7be233135b4b30f2a1d9c78557b28af3ad16ed33
thomasbubi <4480324+thomasbubi@users.noreply.github.com>
2020-02-06 01:47:19 +0100

    added support for command-line arguments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
-
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello Sparkles!" << endl;
+    bool printed_dog = false;
+
+    //iterate over the command-line arguments - http://www.cplusplus.com/articles/DEN36Up4/
+    for(int i=1; i<argc; i++){
+        std::string argument = argv[i];
+        if(argument == "--dog"){
+            std::cout << "woof, woof 🐶" << std::endl;
+            printed_dog = true;
+        }
+    }
+
+    //always print an animal emojie - if dog isn't desired, print a cat
+    if(!printed_dog){
+        std::cout << "meow 🐱" << std::endl;//
+    }
+
     return 0;
 }
