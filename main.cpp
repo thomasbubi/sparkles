6751a24f69ce7a147f69313673db53600da24555
thomasbubi <4480324+thomasbubi@users.noreply.github.com>
2020-02-13 00:20:48 +0100

    create png image file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "external/lodepng.h"
 
 int main(int argc, char* argv[])
 {
     bool printed_dog = false;
-    int width = 640;
-    int height = 480;
+    unsigned long width = 640;
+    unsigned long height = 480;
+    std::string filename = "output.png";
 
     //iterate over the command-line arguments - http://www.cplusplus.com/articles/DEN36Up4/
     for(int i=1; i<argc; i++){
@@ -19,17 +21,27 @@ int main(int argc, char* argv[])
 
         if( (argument == "--width" || argument == "-w") && (i+1 < argc) ){
             std::string next_argument = argv[i+1];
-            int width_from_argument = std::stoi(next_argument);
+            unsigned long width_from_argument = std::stoul(next_argument);
             if(width_from_argument > 0){ width = width_from_argument; }
         }
 
         if( (argument == "--height" || argument == "-h") && (i+1 < argc) ){
             std::string next_argument = argv[i+1];
-            int height_from_argument = std::stoi(next_argument);
+            unsigned long height_from_argument = std::stoul(next_argument);
             if(height_from_argument > 0){ height = height_from_argument; }
         }
 
     }
+
+    unsigned int final_width = static_cast<unsigned int>(width);
+    unsigned int final_height = static_cast<unsigned int>(height);
+
+    std::vector<unsigned char> image(final_width * final_height * 4, 255);
+
+    //conversion from string (command-liune argumewnts of width or height) yields an unsigned long
+    //lodepng::encode expects a unsigned int though
+    unsigned error = lodepng::encode( filename.c_str(), image, final_width, final_height );
+    std::cout << "PNG error: " << lodepng_error_text( error ) << std::endl;
 
     //always print an animal emoji - if dog isn't desired, print a cat
     if(!printed_dog){
