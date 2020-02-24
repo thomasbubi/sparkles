c9ff3f391756d46b80de44a722a801bb2a7dee24
thomasbubi <4480324+thomasbubi@users.noreply.github.com>
2020-02-24 19:57:13 +0100

    Bugfix: normals can contain negative numbers which were handled wrong, yielding in a visible equator and prime meridian on spheres

--- materials/normalmaterial.cpp
+++ materials/normalmaterial.cpp
@@ -9,9 +9,9 @@ NormalMaterial::NormalMaterial()
 
 Color NormalMaterial::shade(const Ray &ray, const Vector3 &intersection_point, const Vector3 &normal) const
 {
-    double r = normal.x();
-    double g = normal.y();
-    double b = normal.z();
+    double r = 0.5 + normal.x() / 2;
+    double g = 0.5 + normal.y() / 2;
+    double b = 0.5 + normal.z() / 2;
     return Color(r,g,b,1);
 }
 
