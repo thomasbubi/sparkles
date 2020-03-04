6036d87b00d9210a886ee4979bfbe0b661bef0e0
thomasbubi <4480324+thomasbubi@users.noreply.github.com>
2020-03-04 22:38:22 +0100

    added mask member variable

--- materials/mixmaterial.h
+++ materials/mixmaterial.h
@@ -2,6 +2,7 @@
 #define MIXMATERIAL_H
 
 #include "material.h"
+#include "../textures/texture.h"
 
 namespace sparkles {
 
@@ -10,7 +11,14 @@ class MixMaterial : public Material
     Material* material_a_;
     Material* material_b_;
     double mix_value_;
+    Texture* mask_;
+    Color rgb_to_bw_;
 public:
+    inline MixMaterial(Material* mat_a, Material* mat_b){
+        material_a_ = mat_a;
+        material_b_ = mat_b;
+        mix_value_ = 0.5;
+    }
     inline MixMaterial(Material* mat_a, Material* mat_b, double value){
         material_a_ = mat_a;
         material_b_ = mat_b;
@@ -26,10 +34,24 @@ public:
         const Vector3& normal,
         unsigned int current_recursion_depth,
         unsigned int max_recursion_depth
-            ) const {
-        return  material_a_->shade(ray, intersection_point, normal, current_recursion_depth, max_recursion_depth) * (1-mix_value_) +
-                material_b_->shade(ray, intersection_point, normal, current_recursion_depth, max_recursion_depth) * mix_value_;
+    ) const {
+        double pixel_value = mix_value_;
+        if(mask_){
+            Color col = mask_->get_value_at(intersection_point.x(), intersection_point.y());
+            pixel_value = col.r() * 1/3 + col.g() * 1/3 + col.b() * 1/3;
+            //todo use rgb_to_bw_
+        }
+        return  material_a_->shade(ray, intersection_point, normal, current_recursion_depth, max_recursion_depth) * (1-pixel_value) +
+                material_b_->shade(ray, intersection_point, normal, current_recursion_depth, max_recursion_depth) * pixel_value;
 
+    }
+    inline void set_mask(Texture* mask){
+        mask_ = mask;
+        rgb_to_bw_ = Color(1/3, 1/3, 1/3);
+    }
+    inline void set_mask(Texture* mask, const Color& rgb_to_bw){
+        mask_ = mask;
+        rgb_to_bw_ = rgb_to_bw;
     }
 };
 
